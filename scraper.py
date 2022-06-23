460df8d648953e3cdbc73b8d61c44d68944dea11
wassname <git@wassname.org>
2022-06-23 20:40:11 +0800

    working

--- scraper.py
+++ scraper.py
@@ -1,10 +1,24 @@
 from unicodedata import name
 import pandas as pd
-import requests
+from requests.adapters import HTTPAdapter, Retry
+import requests_cache
+from pathlib import Path
 import re
 import csv
 import lxml
 from bs4 import BeautifulSoup
+from tqdm.auto import tqdm
+import logging, sys
+
+logger = logging.getLogger(__file__)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+# cache and retry
+session = requests_cache.CachedSession('.demo_cache')
+retries = Retry(total=5,
+                backoff_factor=0.1,
+                status_forcelist=[ 500, 502, 503, 504 ])
+session.mount('http://', HTTPAdapter(max_retries=retries))
 
 sec_url = 'https://www.sec.gov'
 
@@ -14,26 +28,30 @@ def get_request(url):
         'Accept-Encoding': 'gzip, deflate, br',
         'HOST': 'www.sec.gov',
     }
-    return requests.get(url, headers=headers)
+    return session.get(url, headers=headers)
 
 def create_url(cik):
     return 'https://www.sec.gov/cgi-bin/browse-edgar?CIK={}&owner=exclude&action=getcompany&type=13F-HR'.format(cik)
 
 def get_user_input():
-    cik = input("Enter 10-digit CIK number: ")
+    cik = eval(input("Enter 10-digit CIK number: "))
     return cik
 
 
-def scrap_company_report(requested_cik):
+def scrap_company_report(requested_cik, name):
     # Find mutual fund by CIK number on EDGAR
-    response = get_request(create_url(requested_cik))
+    url = create_url(requested_cik)
+    logger.debug(f"index '{url}'")
+    response = get_request(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    tags = soup.findAll('a', id="documentsbutton")
-
-    last_report = (sec_url + tags[0]['href'])
-    previous_report = (sec_url + tags[1]['href'])
-    scrap_report_by_url(last_report, "last_report")
-    scrap_report_by_url(previous_report, "previous_report")
+    main = soup.find(id="seriesDiv")
+    rows = main.findAll('tr')[1:] # skip header
+    for row in rows[:4]:
+        date = row.findAll('td')[3].text
+        tag = row.find('a', id="documentsbutton")
+        last_report = (sec_url + tag['href'])
+        logger.debug(f"scrap_report_by_url '{last_report}' '{name}/{date}.csv'")
+        scrap_report_by_url(last_report, f"{name}/{date}")
 
 
 def scrap_report_by_url(url, filename):
@@ -81,8 +99,46 @@ def xml_to_csv(soup_xml, name):
         df = df.append(row, ignore_index=True)
 
 
-    df.to_csv(f"{name}.csv")
+    fo = Path(f"output/{name}.csv")
+    fo.parent.mkdir(exist_ok=True)
+    df.to_csv(fo)
 
-
-requested_cik = get_user_input()
-scrap_company_report(requested_cik)
+# List of Investments
+CIK_LIST = [{
+    'name': 'Buffett',
+    'cik': '0001067983'
+}, {
+    'name': 'JPMorgan',
+    'cik': '0000019617'
+}, {
+    'name': 'Bridgewater',
+    'cik': '0001350694'
+}, {
+    'name': 'Renaissance',
+    'cik': '0001037389'
+}, {
+    'name': 'TwoSigma',
+    'cik': '0001179392'
+}, {
+    'name': 'DEShaw',
+    'cik': '0001009207'
+}, {
+    'name': 'Millenium',
+    'cik': '0001273087'
+}, {
+    'name': 'Bluecrest',
+    'cik': '0001610880'
+}, {
+    'name': 'AQR',
+    'cik': '0001167557'
+},{
+    'name': 'Scion Asset Management',
+    'cik': '0001649339'
+},
+{
+    'name': 'Burry Michael J',
+    'cik': '0001342573'
+},
+]
+for row in tqdm(CIK_LIST):
+    scrap_company_report(row['cik'], row['name'])
